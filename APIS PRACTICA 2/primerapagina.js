3209603b9bc747ecf219f9d1483f34c40ef3d33f
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-06-21 13:33:27 -0600

    actulizacion 21/06 1:33 pm

--- APIS PRACTICA 2/primerapagina.js
+++ APIS PRACTICA 2/primerapagina.js
@@ -1,21 +1,30 @@
-document.getElementById('askButton').addEventListener('click',
-    async function () {
-        await fetch('https://yesno.wtf/api')
-            .then(response => response.json())
-            .then(data => {
-                const answerDiv = document.getElementById('answer');
+const askButton = document.getElementById('askButton');
+const defaultColor = "linear-gradient(to right, rgb(0, 0, 0), rgb(0, 0, 0), rgb(0, 68, 255))";
+const lightBgColor = "linear-gradient(to right, rgb(229, 229, 229), rgb(0, 68, 255), #04f)";
 
-                answerDiv.innerHTML = '';
-                answerDiv.innerHTML = `
-                <div class=card>
+askButton.addEventListener('click', async function () {
+    await fetch('https://yesno.wtf/api')
+        .then(response => response.json())
+        .then(data => {
+            const answerDiv = document.getElementById('answer');
+            answerDiv.innerHTML = '';
+            answerDiv.innerHTML = `
+                <div class="card">
                     <h2 class="card-title text-center pt-2">${data.answer.toUpperCase()}</h2>
-                    <img src="${data.image}" style="width: 200px class="img-fluid gifperso p-3" alt="Respuesta de ${data.answer}">
+                    <img src="${data.image}" style="width: 200px;" class="img-fluid align-self-center rounded-circle gifperso p-3" alt="Respuesta de ${data.answer}">
                 </div>
             `;
-            })
-            .catch(error => {
-                console.error('Error al obtener la respuesta:', error);
-                const answerDiv = document.getElementById('answer');
-                answerDiv.innerHTML = `<p>Error al obtener la respuesta. Por favor, inténtalo de nuevo.</p>`;
-            });
-    });
+        })
+        .catch(error => {
+            console.error('Error al obtener la respuesta:', error);
+            const answerDiv = document.getElementById('answer');
+            answerDiv.innerHTML = `<p>Error al obtener la respuesta. Por favor, inténtalo de nuevo.</p>`;
+        });
+
+    const bg = document.querySelector('.tacosDePastor');
+    if (bg.style.background === "" || bg.style.background === defaultColor) {
+        bg.style.background = lightBgColor;
+    } else {
+        bg.style.background = defaultColor;
+    }
+});
