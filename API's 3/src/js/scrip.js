4ad344ff73e91a9a403289076e1374ad904d61d5
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-06-24 23:06:15 -0600

    lista de usuario completa

--- API's 3/src/js/scrip.js
+++ API's 3/src/js/scrip.js
@@ -1,11 +1,10 @@
-const pagination = document.querySelector('.pagination');
-const urlBase = 'https://reqres.in/api/users?page=';
+document.addEventListener('DOMContentLoaded', function () {
+    const pagination = document.querySelector('.pagination');
+    const urlBase = 'https://reqres.in/api/users?page=';
 
-pagination.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
-        const pageNumber = e.target.textContent;
+    async function loadUsers(pageNumber) {
         const url = urlBase + pageNumber;
-        fetch(url)
+        await fetch(url)
             .then(response => response.json())
             .then(data => {
                 const user = document.getElementById('root');
@@ -15,22 +14,33 @@ pagination.addEventListener('click', (e) => {
                     div.classList.add('col-md-4');
                     div.classList.add('mb-3');
                     div.innerHTML = `
-            <div class="card h-100">
-            <div class="row g-0">
-            <div class="col-md-4">
-              <img src="${element.avatar}" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">${element.first_name} ${element.last_name}</h5>
-                <p class="card-text">${element.email}</p>
-              </div>
-            </div>
-          </div>
-          
-          `;
+                        <div class="card h-100">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="${element.avatar}" class="img-fluid rounded-start" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${element.first_name} ${element.last_name}</h5>
+                                        <p class="card-text">${element.email}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
                     user.append(div);
                 });
             });
     }
+
+    // Cargar la primera página de usuarios al cargar la página
+    loadUsers(1);
+
+    // Event listener para el clic en la paginación
+    pagination.addEventListener('click', (e) => {
+        if (e.target.tagName === 'A') {
+            const pageNumber = e.target.textContent;
+            loadUsers(pageNumber);
+        }
+    });
 });
